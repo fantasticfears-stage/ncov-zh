5d810b3d59fc2da95a63f5254c64efe1ffbe754b
Erick Guan <297343+erickguan@users.noreply.github.com>
2020-04-22 11:05:17 +0800

    fix data pipeline

--- scripts/transform_province.py
+++ scripts/transform_province.py
@@ -2,6 +2,42 @@ import os
 import pandas
 from csvdata import OUTPUT_PATH, AREA_FILENAME, PROVINCE_FILENAME
 
+PROVINCES = [
+  '河北省',
+  '山西省',
+  '辽宁省',
+  '吉林省',
+  '黑龙江省',
+  '江苏省',
+  '浙江省',
+  '安徽省',
+  '福建省',
+  '江西省',
+  '山东省',
+  '河南省',
+  '湖北省',
+  '湖南省',
+  '广东省',
+  '海南省',
+  '四川省',
+  '贵州省',
+  '云南省',
+  '陕西省',
+  '甘肃省',
+  '青海省',
+  '台湾',
+  '内蒙古自治区',
+  '广西壮族自治区',
+  '西藏自治区',
+  '宁夏回族自治区',
+  '新疆维吾尔自治区',
+  '北京市',
+  '天津市',
+  '上海市',
+  '重庆市',
+  '香港',
+  '澳门'
+]
 
 def transform_province():
   df = pandas.read_csv(os.path.join(OUTPUT_PATH, AREA_FILENAME))
@@ -25,6 +61,7 @@ def transform_province():
           'province_deadCount': 'deceased',
           'updateTime': 'updatedAtDate'
       })
+  df = df[df.name.str.contains("|".join(PROVINCES), regex=True)]
 
   # %%
   df.to_csv(os.path.join(OUTPUT_PATH, PROVINCE_FILENAME), index=False)
