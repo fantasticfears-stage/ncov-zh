e2aac8113a0de37c280724f0379557d9722dece1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-17 00:12:10 +0000

    Deploy ncov-gh.dvis.xyz to github.com/fantasticfears-stage/ncov-zh.git:gh-pages

---
+++ precache-manifest.4c9a9962a94a410faa4e2db0bc1fb164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca0cfb0e3f8251c76610ca1e3a56d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d00e8c8ea0f15f917c",
+    "url": "/static/css/main.7e6cea90.chunk.css"
+  },
+  {
+    "revision": "fe7095d11595f3e653c6",
+    "url": "/static/js/2.65f13262.chunk.js"
+  },
+  {
+    "revision": "dc241fd8a3c12e1408b23c876fb70e60",
+    "url": "/static/js/2.65f13262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d00e8c8ea0f15f917c",
+    "url": "/static/js/main.dc2639af.chunk.js"
+  },
+  {
+    "revision": "1fcb47db654c39d53941",
+    "url": "/static/js/runtime-main.3c9d18eb.js"
+  }
+]);
